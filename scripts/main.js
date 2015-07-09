de664cb1743dced13176834a58d81140b18a5f3d
Taylor Aull <aull.taylor@gmail.com>
2015-07-08 23:32:46 -0400

    Created index and business profile routes

---
+++ scripts/main.js
@@ -0,0 +1,11 @@
+'use strict';
+
+//Initialize Parse
+
+Parse.initialize("AfNszK8Rd7zcsyJhCHpyoRCPM338C6aiylN6mgC6", "XpbpBBz1LrLGmW45ZQOHHolJz1iQIeMoMjRSulII");
+
+
+
+$('#nav-toggle').click(function(){
+	$('#nav-icon1').toggleClass('open');
+});
