99fa1bc2fbcf88203eaea489896f88c1154d586e
Taylor Aull <aull.taylor@gmail.com>
2015-07-14 15:35:09 -0400

    Few styles changes and added few things to the homepage

--- scripts/main.js
+++ scripts/main.js
@@ -8,4 +8,9 @@ Parse.initialize("AfNszK8Rd7zcsyJhCHpyoRCPM338C6aiylN6mgC6", "XpbpBBz1LrLGmW45ZQ
 
 $('#nav-toggle').click(function(){
 	$('#nav-icon1').toggleClass('open');
+	$('.slideout').toggleClass('toggle-slideout');
+	$('.profile-container').toggleClass('profile-container-shift');
+	$('.profile-right').toggleClass('profile-right-shift');
+
+
 });
